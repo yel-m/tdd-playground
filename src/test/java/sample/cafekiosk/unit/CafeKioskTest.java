ef4ee37a34745afd75ad91e4a26bfd7c6884361f
yel-m <coin6442@gmail.com>
2025-01-13 00:34:46 +0900

    chore : displayName 추가

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -1,5 +1,6 @@
 package sample.cafekiosk.unit;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
@@ -19,6 +20,7 @@ class CafeKioskTest {
         System.out.println(">>> 담긴 음료: "+cafeKiosk.getBeverages().get(0).getName());
     }
 
+    @DisplayName("음료 1개를 추가하면 주문 목록에 담긴다.")
     @Test
     void add() {
         CafeKiosk cafeKiosk = new CafeKiosk();
