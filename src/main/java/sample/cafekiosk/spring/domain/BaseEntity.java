268595815c17c30552d32af9f958a06160b891a3
yel-m <coin6442@gmail.com>
2025-01-15 00:05:53 +0900

    add : BaseEntity 생성

---
+++ src/main/java/sample/cafekiosk/spring/domain/BaseEntity.java
@@ -0,0 +1,25 @@
+package sample.cafekiosk.spring.domain;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.cglib.core.Local;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDateTime;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDateTime;
+
+}
