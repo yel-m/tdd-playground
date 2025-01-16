55bb1cbb5a5acba328223cc90e5ac43c90442cdc
yel-m <coin6442@gmail.com>
2025-01-17 02:23:39 +0900

    add : 주문 생성 컨트롤러 및 DTO 생성 (테스트 준비)

---
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/OrderCreateRequest.java
@@ -0,0 +1,8 @@
+package sample.cafekiosk.spring.api.controller.order;
+
+import java.util.List;
+
+public class OrderCreateRequest {
+
+    private List<String> productNumbers;
+}
--- src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
@@ -1,7 +1,6 @@
-package sample.cafekiosk.spring.api.controller;
+package sample.cafekiosk.spring.api.controller.product;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sample.cafekiosk.spring.api.service.ProductResponse;
