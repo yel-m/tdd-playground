baf1c9a6e3e6dad087bead4af7be3c904b303144
yel-m <coin6442@gmail.com>
2025-02-20 10:40:21 +0900

    test : 상품 저장 로직 테스트

--- src/test/java/sample/cafekiosk/spring/api/service/ProductServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/ProductServiceTest.java
@@ -11,7 +11,10 @@ import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import sample.cafekiosk.spring.domain.product.ProductType;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.tuple;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
 import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 
@@ -51,6 +54,14 @@ class ProductServiceTest {
         assertThat(productResponse)
                 .extracting("productNumber", "type", "sellingStatus", "name", "price")
                 .contains("002", HANDMADE, SELLING, "카푸치노", 5000);
+
+        List<Product> products = productRepository.findAll();
+        assertThat(products).hasSize(2)
+                .extracting("productNumber", "type", "sellingStatus", "name", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", HANDMADE, SELLING, "아메리카노", 4000),
+                        tuple("002", HANDMADE, SELLING, "카푸치노", 5000)
+                );
      }
 
     @DisplayName("상품이 하나도 없는 경우 신규 상품을 등록하면 상품번호는 001이다.")
@@ -71,6 +82,13 @@ class ProductServiceTest {
         assertThat(productResponse)
                 .extracting("productNumber", "type", "sellingStatus", "name", "price")
                 .contains("001", HANDMADE, SELLING, "카푸치노", 5000);
+
+        List<Product> products = productRepository.findAll();
+        assertThat(products).hasSize(1)
+                .extracting("productNumber", "type", "sellingStatus", "name", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", HANDMADE, SELLING, "카푸치노", 5000)
+                );
     }
 
     private Product createProduct(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
